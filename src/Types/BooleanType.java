1116daa06d8f225021fc2c37c085d6821c622b66
Tansen Zhu <tansen.zhu@gmail.com>
2014-10-13 23:17:20 -0700

    Fixed up rc.cup re: creating Types

--- src/Types/BooleanType.java
+++ src/Types/BooleanType.java
@@ -1,7 +1,7 @@
 package Types;
 
 public class BooleanType extends Type{
-	public BooleanType(String strName, int size){
-		super(strName, size);
+	public BooleanType(String strName){
+		super(strName, 4);
 	}
 }
