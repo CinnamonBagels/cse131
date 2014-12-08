8d3209d81e279bdbf590d1d027204251e9d4892d
cinnamonbagels <samuelsko@gmail.com>
2014-12-07 21:23:12 -0800

    might work

--- rc.s
+++ rc.s
@@ -23,6 +23,12 @@ float_3:        .single      0r100.001
 float_4:        .single      0r1.2       
 main_cx3:       .word        0           
 main_cx6:       .word        0           
+main_cy3:       .single      0r0.0       
+main_cy6:       .single      0r0.0       
+float_5:        .single      0r4.45      
+float_6:        .single      0r4.45      
+main_cz3:       .word        0           
+main_cz6:       .word        0           
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -233,6 +239,228 @@ branchEnd_2:
     ld          [%l1], %l0
     st          %l0, [%l0]
 /* Done. */
+/* Printing int */
+    set         _intFmt, %o0
+    call    printf
+    nop
+/* Done printing int. */
+/* printf on int */
+    set         _intFmt, %o0
+    set         4, %o1
+    call    printf
+    nop
+
+/* Printing int */
+    set         _intFmt, %o0
+    call    printf
+    nop
+/* Done printing int. */
+/* printf on int */
+    set         _intFmt, %o0
+    set         4, %o1
+    call    printf
+    nop
+
+/* Printing int */
+    set         _intFmt, %o0
+    call    printf
+    nop
+/* Done printing int. */
+/* Printing int */
+    set         _intFmt, %o0
+    call    printf
+    nop
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* setting cy1 = y3 */
+    set         -24, %l0
+    add         %fp, %l0, %l0
+/* setting float */
+    set         float_0, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* Done. */
+/* setting cy5 = y1 */
+    set         -28, %l0
+    add         %fp, %l0, %l0
+    set         y1, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* Done. */
+/* printing float */
+/* Loading Variable */
+    set         -24, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* Done loading variable. */
+    call    printFloat
+    nop
+/* Done printing float. */
+    set         float_5, %l0
+    ld          [%l0], %f0
+    call    printFloat
+    nop
+
+/* printing float */
+/* Loading Variable */
+    set         main_cy3, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+/* Done loading variable. */
+    call    printFloat
+    nop
+/* Done printing float. */
+    set         float_6, %l0
+    ld          [%l0], %f0
+    call    printFloat
+    nop
+
+/* printing float */
+/* Loading Variable */
+    set         -28, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* Done loading variable. */
+    call    printFloat
+    nop
+/* Done printing float. */
+/* printing float */
+/* Loading Variable */
+    set         main_cy6, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+/* Done loading variable. */
+    call    printFloat
+    nop
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* setting cz1 = z3 */
+    set         -32, %l0
+    add         %fp, %l0, %l0
+    set         0, %l1
+    st          %l1, [%l0]
+/* Done. */
+/* setting cz5 = z1 */
+    set         -36, %l0
+    add         %fp, %l0, %l0
+    set         z1, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l0
+    st          %l0, [%l0]
+/* Done. */
+/* Printing bool */
+/* Loading Variable */
+    set         -32, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+/* Done loading variable. */
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_3
+    nop
+printTrue_3:
+    set         _boolT, %o1
+    ba      branchEnd_3
+    nop
+printFalse_3:
+    set         _boolF, %o1
+branchEnd_3:
+    call    printf
+    nop
+/* Done printing bool. */
+    set         _intFmt, %o0
+    set         _boolF, %o1
+    call    printf
+    nop
+
+/* Printing bool */
+/* Loading Variable */
+    set         main_cz3, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l0
+/* Done loading variable. */
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_4
+    nop
+printTrue_4:
+    set         _boolT, %o1
+    ba      branchEnd_4
+    nop
+printFalse_4:
+    set         _boolF, %o1
+branchEnd_4:
+    call    printf
+    nop
+/* Done printing bool. */
+    set         _intFmt, %o0
+    set         _boolF, %o1
+    call    printf
+    nop
+
+/* Printing bool */
+/* Loading Variable */
+    set         -36, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+/* Done loading variable. */
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_5
+    nop
+printTrue_5:
+    set         _boolT, %o1
+    ba      branchEnd_5
+    nop
+printFalse_5:
+    set         _boolF, %o1
+branchEnd_5:
+    call    printf
+    nop
+/* Done printing bool. */
+/* Printing bool */
+/* Loading Variable */
+    set         main_cz6, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l0
+/* Done loading variable. */
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_6
+    nop
+printTrue_6:
+    set         _boolT, %o1
+    ba      branchEnd_6
+    nop
+printFalse_6:
+    set         _boolF, %o1
+branchEnd_6:
+    call    printf
+    nop
+/* Done printing bool. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* Storing x2 into x1 */
+    set         x1, %l0
+    add         %g0, %l0, %l0
+    set         x2, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %l1
+    st          %l1, [%l0]
+/* Printing int */
+    set         _intFmt, %o0
+    call    printf
+    nop
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
     ret 
     restore
-SAVE.main = -(92 + 20) & -8
+SAVE.main = -(92 + 36) & -8
