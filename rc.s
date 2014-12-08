48311ced70bb1912a3c204fe370c4b1f7b3dfc40
cinnamonbagels <samuelsko@gmail.com>
2014-12-07 21:32:42 -0800

    might work

--- rc.s
+++ rc.s
@@ -238,14 +238,6 @@ branchEnd_2:
     set         4, %l1
     st          %l1, [%l0]
 /* Done. */
-/* setting cx5 = x1 */
-    set         -20, %l0
-    add         %fp, %l0, %l0
-    set         x1, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %l0
-    st          %l0, [%l0]
-/* Done. */
     ret 
     restore
-SAVE.main = -(92 + 20) & -8
+SAVE.main = -(92 + 16) & -8
