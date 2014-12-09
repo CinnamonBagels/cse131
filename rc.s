872eaf40ffb7a8c90491713e19d817583164f403
cinnamonbagels <samuelsko@gmail.com>
2014-12-08 17:11:22 -0800

    garos test

--- rc.s
+++ rc.s
@@ -1,3 +1,10 @@
+! --globals--
+                .section     ".data"
+                .align 4
+float_0:        .single      0r420.25    
+float_1:        .single      0r662.5     
+float_2:        .single      0r1082.75   
+str_3:          .asciz       "\n"        
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -14,111 +21,44 @@ rfmt:           .asciz       "%.21f"
 main:
     set         SAVE.main, %g1
     save        %sp, %g1, %sp
-/* setting huit = 8 */
-    set         -4, %l0
-    add         %fp, %l0, %l0
-    set         8, %l1
-    st          %l1, [%l0]
-/* Done. */
-/* setting trois = 3 */
-    set         -8, %l0
-    add         %fp, %l0, %l0
-    set         3, %l1
-    st          %l1, [%l0]
-/* Done. */
-/* setting deux = 2 */
-    set         -12, %l0
-    add         %fp, %l0, %l0
-    set         2, %l1
-    st          %l1, [%l0]
-/* Done. */
-/* setting un = 1 */
-    set         -16, %l0
-    add         %fp, %l0, %l0
-    set         1, %l1
-    st          %l1, [%l0]
-/* Done. */
-/* Prepping Arithmetic Calculations by loading */
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -1, %l1
-/* Multiplying */
-    mov         %l0, %o0
-    mov         %l1, %o1
-    call    .mul
-    nop
-    mov         %o0, %l0
-    set         -20, %l4
-    add         %fp, %l4, %l4
-    st          %l0, [%l4]
-/* Prepping Arithmetic Calculations by loading */
-    set         -20, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -4, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l1
-/* Multiplying */
-    mov         %l0, %o0
-    mov         %l1, %o1
-    call    .mul
-    nop
-    mov         %o0, %l0
-    set         -24, %l4
-    add         %fp, %l4, %l4
-    st          %l0, [%l4]
-/* Prepping Arithmetic Calculations by loading */
-    set         -24, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -12, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l1
-/* Dividing */
-    mov         %l0, %o0
-    mov         %l1, %o1
-    call    .div
-    nop
-    mov         %o0, %l0
-    set         -28, %l4
-    add         %fp, %l4, %l4
-    st          %l0, [%l4]
-/* Prepping Arithmetic Calculations by loading */
-    set         -28, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -12, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l1
-/* Adding */
-    add         %l0, %l1, %l3
-    set         -32, %l4
-    add         %fp, %l4, %l4
-    st          %l3, [%l4]
-/* Prepping Arithmetic Calculations by loading */
-    set         -32, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -16, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l1
-/* Subtracting */
-    sub         %l0, %l1, %l3
-    set         -36, %l4
-    add         %fp, %l4, %l4
-    st          %l3, [%l4]
-/* Printing int int - int */
+/* printf on int */
     set         _intFmt, %o0
-    set         -36, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %o1
+    set         420, %o1
     call    printf
     nop
-/* Done printing int. */
+
+    set         _endl, %o0
+    call    printf
+    nop
+/* printf on int */
+    set         _intFmt, %o0
+    set         214, %o1
+    call    printf
+    nop
+
+    set         _endl, %o0
+    call    printf
+    nop
+    set         float_2, %l0
+    ld          [%l0], %f0
+    call    printFloat
+    nop
+
+/* printing string */
+    set         _strFmt, %o0
+    set         str_3, %o1
+    call    printf
+    nop
+/* Done printing string. */
+/* printf on int */
+    set         _intFmt, %o0
+    set         4, %o1
+    call    printf
+    nop
+
     set         _endl, %o0
     call    printf
     nop
     ret 
     restore
-SAVE.main = -(92 + 36) & -8
+SAVE.main = -(92 + 0) & -8
