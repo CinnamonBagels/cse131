d6c831998dcf1c73e95140edd60c518079225f5c
cinnamonbagels <samuelsko@gmail.com>
2014-12-11 15:47:05 -0800

    stuff

--- rc.s
+++ rc.s
@@ -1,16 +1,29 @@
 ! --globals--
                 .section     ".data"
                 .align 4
-                 .global     rick,ankur,rickAndAnkur
-rick:           .word        1           
-ankur:          .word        0           
-rickAndAnkur:    .word        0           
+                 .global     f1,f2,f3,i1,i2,i3,b1,b2,b3
+float_0:        .single      0r5.0       
+f1:             .single      0r5.0       
+float_1:        .single      0r6.0       
+f2:             .single      0r6.0       
+f3:             .single      0r0.0       
+i1:             .word        0           
+i2:             .word        0           
+i3:             .word        0           
+b1:             .word        0           
+b2:             .word        0           
+b3:             .word        0           
 globalInit_:    .word        0           
-str_0:          .asciz       "rick is: " 
-str_1:          .asciz       "ankur is: "
-str_2:          .asciz       "rickAndAnkur is: "
-str_3:          .asciz       "a is: "    
-str_4:          .asciz       "b is: "    
+str_2:          .asciz       "f1 is: "   
+str_3:          .asciz       "f2 is: "   
+float_4:        .single      0r7.0       
+str_5:          .asciz       "f3 is: "   
+str_6:          .asciz       "i1 is: "   
+str_7:          .asciz       "i2 is: "   
+str_8:          .asciz       "i3 is: "   
+str_9:          .asciz       "b1 is: "   
+str_10:         .asciz       "b2 is: "   
+str_11:         .asciz       "b3 is: "   
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -37,65 +50,182 @@ main:
     set         1, %l1
     st          %l1, [%l0]
 globalInit_end:
-/* line number 8*/
-/* setting a = true */
-    set         1, %l0
-    st          %l0, [%fp-8]
-/* line number 8*/
-/* Done. */
-/* line number 10*/
-/* setting b = a */
-    set         -12, %l0
-    add         %fp, %l0, %l0
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l2
-    st          %l2, [%l0]
-/* line number 10*/
-/* Done. */
-/* line number 10*/
-/* Storing ankur into rickAndAnkur */
-    set         rickAndAnkur, %l0
-    add         %g0, %l0, %l0
-    set         ankur, %l2
-    add         %g0, %l2, %l2
-    ld          [%l2], %l1
-    st          %l1, [%l0]
-/* line number 11*/
-/* Storing a into ankur */
-    set         ankur, %l0
-    add         %g0, %l0, %l0
-    set         -8, %l2
-    add         %fp, %l2, %l2
-    ld          [%l2], %l1
-    st          %l1, [%l0]
-/* line number 12*/
-/* Storing b into rick */
-    set         rick, %l0
-    add         %g0, %l0, %l0
-    set         -12, %l2
-    add         %fp, %l2, %l2
-    ld          [%l2], %l1
-    st          %l1, [%l0]
-/* line number 13*/
-/* Storing rick into rickAndAnkur */
-    set         rickAndAnkur, %l0
-    add         %g0, %l0, %l0
-    set         rick, %l2
-    add         %g0, %l2, %l2
-    ld          [%l2], %l1
-    st          %l1, [%l0]
 /* line number 15*/
 /* printing string */
     set         _strFmt, %o0
-    set         str_0, %o1
+    set         str_2, %o1
     call    printf
     nop
 /* line number 15*/
 /* Done printing string. */
 /* line number 15*/
-/* Printing bool rick */
-    set         rick, %l1
+/* printing float f1 */
+    set         f1, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 15*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 16*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_3, %o1
+    call    printf
+    nop
+/* line number 16*/
+/* Done printing string. */
+/* line number 16*/
+/* printing float f2 */
+    set         f2, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 16*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+! --storing constant f3 with value 7.0
+    set         f3, %l0
+    add         %g0, %l0, %l0
+    set         float_4, %l1
+    ld          [%l1], %f1
+    st          %f1, [%l0]
+/* line number 19*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_5, %o1
+    call    printf
+    nop
+/* line number 19*/
+/* Done printing string. */
+/* line number 19*/
+/* printing float f3 */
+    set         f3, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 19*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+! --storing constant i1 with value 1.0
+    set         i1, %l0
+    add         %g0, %l0, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+! --storing constant i2 with value 2.0
+    set         i2, %l0
+    add         %g0, %l0, %l0
+    set         2, %l1
+    st          %l1, [%l0]
+/* line number 23*/
+/* Storing i1 into i3 */
+    set         i3, %l0
+    add         %g0, %l0, %l0
+    set         i1, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %l1
+    st          %l1, [%l0]
+/* line number 25*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_6, %o1
+    call    printf
+    nop
+/* line number 25*/
+/* Done printing string. */
+/* line number 25*/
+/* Printing int i1 */
+    set         _intFmt, %o0
+    set         i1, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 25*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 26*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_7, %o1
+    call    printf
+    nop
+/* line number 26*/
+/* Done printing string. */
+/* line number 26*/
+/* Printing int i2 */
+    set         _intFmt, %o0
+    set         i2, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 26*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 27*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_8, %o1
+    call    printf
+    nop
+/* line number 27*/
+/* Done printing string. */
+/* line number 27*/
+/* Printing int i3 */
+    set         _intFmt, %o0
+    set         i3, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 27*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+! --storing constant b1 with value 1.0
+    set         b1, %l0
+    add         %g0, %l0, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+! --storing constant b2 with value 0.0
+    set         b2, %l0
+    add         %g0, %l0, %l0
+    set         0, %l1
+    st          %l1, [%l0]
+/* line number 31*/
+/* Storing b2 into b3 */
+    set         b3, %l0
+    add         %g0, %l0, %l0
+    set         b2, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %l1
+    st          %l1, [%l0]
+/* line number 33*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_9, %o1
+    call    printf
+    nop
+/* line number 33*/
+/* Done printing string. */
+/* line number 33*/
+/* Printing bool b1 */
+    set         b1, %l1
     add         %g0, %l1, %l1
     ld          [%l1], %l0
     set         _strFmt, %o0
@@ -111,22 +241,22 @@ printFalse_0:
 branchEnd_0:
     call    printf
     nop
-/* line number 15*/
+/* line number 33*/
 /* Done printing bool. */
     set         _endl, %o0
     call    printf
     nop
-/* line number 16*/
+/* line number 34*/
 /* printing string */
     set         _strFmt, %o0
-    set         str_1, %o1
+    set         str_10, %o1
     call    printf
     nop
-/* line number 16*/
+/* line number 34*/
 /* Done printing string. */
-/* line number 16*/
-/* Printing bool ankur */
-    set         ankur, %l1
+/* line number 34*/
+/* Printing bool b2 */
+    set         b2, %l1
     add         %g0, %l1, %l1
     ld          [%l1], %l0
     set         _strFmt, %o0
@@ -142,22 +272,22 @@ printFalse_1:
 branchEnd_1:
     call    printf
     nop
-/* line number 16*/
+/* line number 34*/
 /* Done printing bool. */
     set         _endl, %o0
     call    printf
     nop
-/* line number 17*/
+/* line number 35*/
 /* printing string */
     set         _strFmt, %o0
-    set         str_2, %o1
+    set         str_11, %o1
     call    printf
     nop
-/* line number 17*/
+/* line number 35*/
 /* Done printing string. */
-/* line number 17*/
-/* Printing bool rickAndAnkur */
-    set         rickAndAnkur, %l1
+/* line number 35*/
+/* Printing bool b3 */
+    set         b3, %l1
     add         %g0, %l1, %l1
     ld          [%l1], %l0
     set         _strFmt, %o0
@@ -173,69 +303,7 @@ printFalse_2:
 branchEnd_2:
     call    printf
     nop
-/* line number 17*/
-/* Done printing bool. */
-    set         _endl, %o0
-    call    printf
-    nop
-/* line number 18*/
-/* printing string */
-    set         _strFmt, %o0
-    set         str_3, %o1
-    call    printf
-    nop
-/* line number 18*/
-/* Done printing string. */
-/* line number 18*/
-/* Printing bool a */
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         _strFmt, %o0
-    cmp         %l0, %g0
-    be      printFalse_3
-    nop
-printTrue_3:
-    set         _boolT, %o1
-    ba      branchEnd_3
-    nop
-printFalse_3:
-    set         _boolF, %o1
-branchEnd_3:
-    call    printf
-    nop
-/* line number 18*/
-/* Done printing bool. */
-    set         _endl, %o0
-    call    printf
-    nop
-/* line number 19*/
-/* printing string */
-    set         _strFmt, %o0
-    set         str_4, %o1
-    call    printf
-    nop
-/* line number 19*/
-/* Done printing string. */
-/* line number 19*/
-/* Printing bool b */
-    set         -12, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         _strFmt, %o0
-    cmp         %l0, %g0
-    be      printFalse_4
-    nop
-printTrue_4:
-    set         _boolT, %o1
-    ba      branchEnd_4
-    nop
-printFalse_4:
-    set         _boolF, %o1
-branchEnd_4:
-    call    printf
-    nop
-/* line number 19*/
+/* line number 35*/
 /* Done printing bool. */
     set         _endl, %o0
     call    printf
@@ -243,4 +311,4 @@ branchEnd_4:
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 12) & -8
+SAVE.main = -(92 + 4) & -8
