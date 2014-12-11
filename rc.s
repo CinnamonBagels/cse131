0b25153ea490fee8a700b8c090537dc99fc573f4
cinnamonbagels <samuelsko@gmail.com>
2014-12-10 23:16:45 -0800

    working

--- rc.s
+++ rc.s
@@ -75,10 +75,7 @@ rec_x:          .word        0
 staticGuard_rec_x:    .word        0           
 str_68:         .asciz       "REC: "     
 str_69:         .asciz       "AFTER"     
-main_x:         .word        0           
-staticGuard_main_x:    .word        0           
-str_70:         .asciz       "REC: "     
-str_71:         .asciz       "BAD"       
+str_70:         .asciz       "BAD"       
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -1697,85 +1694,11 @@ SAVE.rec = -(92 + 12) & -8
 main:
     set         SAVE.main, %g1
     save        %sp, %g1, %sp
-    set         staticGuard_main_x, %l0
-    ld          [%l0], %l1
-    cmp         %g0, %l1
-    bne     staticGuardLabel_main_x
-    nop
-! --storing constant x with value 0.0
-    set         main_x, %l0
-    add         %g0, %l0, %l0
-    set         0, %l1
-    st          %l1, [%l0]
-    set         staticGuard_main_x, %l2
-    set         1, %l3
-    st          %l3, [%l2]
-staticGuardLabel_main_x:
-/* Incrementing */
-    set         main_x, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %l0
-    inc     %l0
-    set         main_x, %l2
-    add         %g0, %l2, %l2
-    st          %l0, [%l2]
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    st          %l0, [%l1]
-/* printing string */
-    set         _strFmt, %o0
-    set         str_70, %o1
-    call    printf
-    nop
-/* Done printing string. */
-/* Printing int x */
-    set         _intFmt, %o0
-    set         main_x, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %o1
-    call    printf
-    nop
-/* Done printing int. */
-    set         _endl, %o0
-    call    printf
-    nop
-/* Prepping Comparison Calculations by loading */
-    set         main_x, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %l0
-    set         5, %l1
-/* Starting Less than */
-    cmp         %l0, %l1
-    bl      less_1
-    nop
-    set         0, %l3
-    ba      lessEnd_1
-    nop
-less_1:
-    set         1, %l3
-lessEnd_1:
-/* Storing result of Comparison Op */
-    set         -12, %l4
-    add         %fp, %l4, %l4
-    st          %l3, [%l4]
-    set         -12, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    cmp         %l0, %g0
-    be      else_2
-    nop
-if_2:
-    call    main
-    nop
-    ba      endIf_3
-    nop
-else_2:
-endIf_3:
     call    foo
     nop
 /* printing string */
     set         _strFmt, %o0
-    set         str_71, %o1
+    set         str_70, %o1
     call    printf
     nop
 /* Done printing string. */
@@ -1785,4 +1708,4 @@ endIf_3:
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 12) & -8
+SAVE.main = -(92 + 4) & -8
