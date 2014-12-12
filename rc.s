9ad453c013cdb7e3266c1c18405e49f481863af4
cinnamonbagels <samuelsko@gmail.com>
2014-12-11 23:57:57 -0800

    weatuawnet

---
+++ rc.s
@@ -0,0 +1,466 @@
+! --globals--
+                .section     ".data"
+                .align 4
+str_0:          .asciz       " "         
+str_1:          .asciz       " "         
+str_2:          .asciz       " "         
+str_3:          .asciz       " "         
+str_4:          .asciz       " "         
+str_5:          .asciz       " "         
+globalInit_:    .word        0           
+
+! DEFINING INTERNAL CONSTANTS --
+                .section     ".rodata"
+_endl:          .asciz       "\n"        
+_intFmt:        .asciz       "%d"        
+_strFmt:        .asciz       "%s"        
+_boolT:         .asciz       "true"      
+_boolF:         .asciz       "false"     
+rfmt:           .asciz       "%.21f"     
+arrayOutOfBounds:    .asciz       "Index value of %d is outside legal range [0,%d)."
+
+                .section     ".text"
+                .align 4
+                .global      main
+main:
+    set         SAVE.main, %g1
+    save        %sp, %g1, %sp
+    set         globalInit_, %l0
+    ld          [%l0], %l0
+    cmp         %l0, %g0
+    bne     globalInit_end
+    nop
+    set         globalInit_, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+globalInit_end:
+/* line number 6*/
+/* setting ten = 10 */
+    set         10, %l0
+    st          %l0, [%fp-8]
+/* line number 6*/
+/* Done. */
+/* line number 8*/
+/* setting twenty = 20 */
+    set         20, %l0
+    st          %l0, [%fp-12]
+/* line number 8*/
+/* Done. */
+/* line number 8*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 8*/
+/* Starting Less than */
+    cmp         %l0, %l1
+    bl      less_0
+    nop
+    set         0, %l3
+    ba      lessEnd_0
+    nop
+less_0:
+    set         1, %l3
+lessEnd_0:
+/* line number 8*/
+/* Storing result of Comparison Op */
+    set         -16, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 8*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: <...
+ */
+    set         -16, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_0
+    nop
+printTrue_0:
+    set         _boolT, %o1
+    ba      branchEnd_0
+    nop
+printFalse_0:
+    set         _boolF, %o1
+branchEnd_0:
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing bool. */
+/* line number 8*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_0, %o1
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing string. */
+/* line number 8*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 8*/
+/* Starting greater than */
+    cmp         %l0, %l1
+    bg      greater_1
+    nop
+    set         0, %l3
+    ba      greaterEnd_1
+    nop
+greater_1:
+    set         1, %l3
+greaterEnd_1:
+/* line number 8*/
+/* Storing result of Comparison Op */
+    set         -20, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 8*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: >...
+ */
+    set         -20, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_1
+    nop
+printTrue_1:
+    set         _boolT, %o1
+    ba      branchEnd_1
+    nop
+printFalse_1:
+    set         _boolF, %o1
+branchEnd_1:
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing bool. */
+/* line number 8*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_1, %o1
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing string. */
+/* line number 8*/
+/* Prepping Comparison Calculations by loading */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 8*/
+/* Starting not equal */
+    cmp         %l0, %l1
+    bne     nEqual_2
+    nop
+    set         0, %l3
+    ba      nEqualEnd_2
+    nop
+nEqual_2:
+    set         1, %l3
+nEqualEnd_2:
+/* line number 8*/
+/* Storing result of Comparison Op */
+    set         -24, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 8*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: !=...
+ */
+    set         -24, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_2
+    nop
+printTrue_2:
+    set         _boolT, %o1
+    ba      branchEnd_2
+    nop
+printFalse_2:
+    set         _boolF, %o1
+branchEnd_2:
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing bool. */
+/* line number 8*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_2, %o1
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing string. */
+/* line number 9*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 9*/
+/* Starting == equal */
+    cmp         %l0, %l1
+    be      equal_3
+    nop
+    set         0, %l3
+    ba      equal_3
+    nop
+equal_3:
+    set         1, %l3
+equalEnd_3:
+/* line number 9*/
+/* Storing result of Comparison Op */
+    set         -28, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 9*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: ==...
+ */
+    set         -28, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_3
+    nop
+printTrue_3:
+    set         _boolT, %o1
+    ba      branchEnd_3
+    nop
+printFalse_3:
+    set         _boolF, %o1
+branchEnd_3:
+    call    printf
+    nop
+/* line number 9*/
+/* Done printing bool. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 12*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 12*/
+/* Starting greater than */
+    cmp         %l0, %l1
+    bg      greater_4
+    nop
+    set         0, %l3
+    ba      greaterEnd_4
+    nop
+greater_4:
+    set         1, %l3
+greaterEnd_4:
+/* line number 12*/
+/* Storing result of Comparison Op */
+    set         -32, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 12*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: >...
+ */
+    set         -32, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_4
+    nop
+printTrue_4:
+    set         _boolT, %o1
+    ba      branchEnd_4
+    nop
+printFalse_4:
+    set         _boolF, %o1
+branchEnd_4:
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing bool. */
+/* line number 12*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_3, %o1
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing string. */
+/* line number 12*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 12*/
+/* Starting Less than */
+    cmp         %l0, %l1
+    bl      less_5
+    nop
+    set         0, %l3
+    ba      lessEnd_5
+    nop
+less_5:
+    set         1, %l3
+lessEnd_5:
+/* line number 12*/
+/* Storing result of Comparison Op */
+    set         -36, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 12*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: <...
+ */
+    set         -36, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_5
+    nop
+printTrue_5:
+    set         _boolT, %o1
+    ba      branchEnd_5
+    nop
+printFalse_5:
+    set         _boolF, %o1
+branchEnd_5:
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing bool. */
+/* line number 12*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_4, %o1
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing string. */
+/* line number 12*/
+/* Prepping Comparison Calculations by loading */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 12*/
+/* Starting == equal */
+    cmp         %l0, %l1
+    be      equal_6
+    nop
+    set         0, %l3
+    ba      equal_6
+    nop
+equal_6:
+    set         1, %l3
+equalEnd_6:
+/* line number 12*/
+/* Storing result of Comparison Op */
+    set         -40, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 12*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: ==...
+ */
+    set         -40, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_6
+    nop
+printTrue_6:
+    set         _boolT, %o1
+    ba      branchEnd_6
+    nop
+printFalse_6:
+    set         _boolF, %o1
+branchEnd_6:
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing bool. */
+/* line number 12*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_5, %o1
+    call    printf
+    nop
+/* line number 12*/
+/* Done printing string. */
+/* line number 13*/
+/* Prepping Comparison Calculations by loading */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 13*/
+/* Starting not equal */
+    cmp         %l0, %l1
+    bne     nEqual_7
+    nop
+    set         0, %l3
+    ba      nEqualEnd_7
+    nop
+nEqual_7:
+    set         1, %l3
+nEqualEnd_7:
+/* line number 13*/
+/* Storing result of Comparison Op */
+    set         -44, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 13*/
+/* Printing bool Validating ComparisonOp int and int as a BooleanType for operator: !=...
+ */
+    set         -44, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    set         _strFmt, %o0
+    cmp         %l0, %g0
+    be      printFalse_7
+    nop
+printTrue_7:
+    set         _boolT, %o1
+    ba      branchEnd_7
+    nop
+printFalse_7:
+    set         _boolF, %o1
+branchEnd_7:
+    call    printf
+    nop
+/* line number 13*/
+/* Done printing bool. */
+    set         _endl, %o0
+    call    printf
+    nop
+main_end:
+    ret 
+    restore
+SAVE.main = -(92 + 44) & -8
