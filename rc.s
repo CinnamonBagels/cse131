98b7721df9b17646fd23cc18b31b3dc2ea89e684
cinnamonbagels <samuelsko@gmail.com>
2014-12-12 01:27:55 -0800

    why

--- rc.s
+++ rc.s
@@ -57,75 +57,7 @@ main:
     set         1, %l1
     st          %l1, [%l0]
 globalInit_end:
-/* line number 8*/
-/* setting k = 20 */
-    set         20, %l0
-    st          %l0, [%fp-8]
-/* line number 8*/
-/* Done. */
-/* line number 8*/
-/* Prepping Arithmetic Calculations by loading */
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -1, %l1
-/* line number 8*/
-/* Multiplying */
-    mov         %l0, %o0
-    mov         %l1, %o1
-    call    .mul
-    nop
-    mov         %o0, %l0
-/* line number 8*/
-/* Storing result of Binary Op */
-    set         -12, %l4
-    add         %fp, %l4, %l4
-    st          %l0, [%l4]
-/* line number 8*/
-/* Prepping Arithmetic Calculations by loading */
-    set         -8, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -1, %l1
-/* line number 8*/
-/* Multiplying */
-    mov         %l0, %o0
-    mov         %l1, %o1
-    call    .mul
-    nop
-    mov         %o0, %l0
-/* line number 8*/
-/* Storing result of Binary Op */
-    set         -16, %l4
-    add         %fp, %l4, %l4
-    st          %l0, [%l4]
-/* line number 8*/
-/* Prepping Arithmetic Calculations by loading */
-    set         -12, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         -16, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l1
-/* line number 8*/
-/* Adding */
-    add         %l0, %l1, %l3
-/* line number 8*/
-/* Storing result of Binary Op */
-    set         -20, %l4
-    add         %fp, %l4, %l4
-    st          %l3, [%l4]
-/* line number 10*/
-/* setting l = int + int */
-    set         -24, %l0
-    add         %fp, %l0, %l0
-    set         -20, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l2
-    st          %l2, [%l0]
-/* line number 10*/
-/* Done. */
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 24) & -8
+SAVE.main = -(92 + 4) & -8
