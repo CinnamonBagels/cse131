339703e64ab6326a436f7e756c202f18de5fd76a
cinnamonbagels <samuelsko@gmail.com>
2014-12-11 16:30:12 -0800

    stuff

--- rc.s
+++ rc.s
@@ -1,15 +1,22 @@
 ! --globals--
                 .section     ".data"
                 .align 4
-                 .global     shakespeare,marlowe
+                 .global     shakespeare,marlowe,goethe,shelley
 shakespeare:    .single      0r0.0       
 marlowe:        .single      0r0.0       
+goethe:         .single      0r0.0       
+shelley:        .single      0r0.0       
 globalInit_:    .word        0           
 float_0:        .single      0r9000.0    
 float_1:        .single      0r5000.0    
-float_2:        .single      0r5.6       
-float_3:        .single      0r9.2       
-float_4:        .single      0r51.52     
+float_2:        .single      0r12000.0   
+float_3:        .single      0r7000.0    
+float_4:        .single      0r5.6       
+float_5:        .single      0r9.2       
+float_6:        .single      0r51.52     
+float_7:        .single      0r1.0       
+float_8:        .single      0r9999.5    
+float_9:        .single      0r1.0       
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -48,63 +55,448 @@ globalInit_end:
     set         float_1, %l1
     ld          [%l1], %f1
     st          %f1, [%l0]
-/* line number 12*/
+! --storing constant goethe with value 12000.0
+    set         goethe, %l0
+    add         %g0, %l0, %l0
+    set         float_2, %l1
+    ld          [%l1], %f1
+    st          %f1, [%l0]
+! --storing constant shelley with value 7000.0
+    set         shelley, %l0
+    add         %g0, %l0, %l0
+    set         float_3, %l1
+    ld          [%l1], %f1
+    st          %f1, [%l0]
+/* line number 14*/
 /* setting twilightSeries = 5000 */
     set         5000, %l0
     st          %l0, [%fp-8]
-/* line number 12*/
+/* line number 14*/
 /* Done. */
-/* line number 12*/
+/* line number 15*/
+/* setting reallyReally = 1000 */
+    set         1000, %l0
+    st          %l0, [%fp-12]
+/* line number 15*/
+/* Done. */
+/* line number 17*/
+/* setting sucksAlot = 9000 */
+    set         9000, %l0
+    st          %l0, [%fp-16]
+/* line number 17*/
+/* Done. */
+/* line number 17*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 17*/
+/* Subtracting */
+    set         shakespeare, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    set         marlowe, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f1
+    fsubs       %f0, %f1, %f3
+/* line number 17*/
+/* Storing result of Binary Op */
+    set         -20, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 17*/
 /* Prepping Arithmetic Calculations by loading */
     set         -8, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %l0
-/* line number 12*/
-/* Subtracting */
+/* line number 17*/
+/* Adding */
     set         -8, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
-/* line number 12*/
+    set         -20, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fitos       %f0, %f0
+    fadds       %f0, %f1, %f3
+/* line number 17*/
+/* Storing result of Binary Op */
+    set         -24, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 17*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 17*/
+/* Subtracting */
+    set         -24, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* line number 17*/
 /* Storing Arithmetic Op into  */
     set         4, %l0
     add         %fp, %l0, %l0
-    set         float_4, %l2
+    set         float_6, %l2
     add         %g0, %l2, %l2
     ld          [%l2], %f1
     st          %f1, [%l0]
     set         4, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f1
-    fitos       %f0, %f0
     fsubs       %f0, %f1, %f3
-/* line number 12*/
+/* line number 17*/
 /* Storing result of Binary Op */
-    set         -12, %l4
+    set         -28, %l4
     add         %fp, %l4, %l4
     st          %f3, [%l4]
-/* line number 14*/
-/* setting byron = int - float */
-    set         -16, %l0
+/* line number 18*/
+/* setting byron = float - float */
+    set         -32, %l0
     add         %fp, %l0, %l0
-    set         -12, %l1
+    set         -28, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
     st          %f0, [%l0]
-/* line number 14*/
+/* line number 18*/
 /* Done. */
-/* line number 14*/
-/* printing float byron */
+/* line number 18*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 18*/
+/* Subtracting */
+    set         -32, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    set         shelley, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f1
+    fsubs       %f0, %f1, %f3
+/* line number 18*/
+/* Storing result of Binary Op */
+    set         -36, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 18*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 18*/
+/* Multiplying */
+    set         -36, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* line number 18*/
+/* Storing 1.0 into  */
+    set         4, %l0
+    add         %fp, %l0, %l0
+    set         float_7, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %f1
+    st          %f1, [%l0]
+    set         4, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fmuls       %f0, %f1, %f3
+/* line number 18*/
+/* Storing result of Binary Op */
+    set         -40, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 18*/
+/* Prepping Arithmetic Calculations by loading */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 18*/
+/* Subtracting */
+    set         -40, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fitos       %f1, %f1
+    fsubs       %f0, %f1, %f3
+/* line number 18*/
+/* Storing result of Binary Op */
+    set         -44, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 19*/
+/* setting tennyson = float - int */
+    set         -48, %l0
+    add         %fp, %l0, %l0
+    set         -44, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* line number 19*/
+/* Done. */
+/* line number 19*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 19*/
+/* Subtracting */
+    set         -48, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    set         -32, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fsubs       %f0, %f1, %f3
+/* line number 19*/
+/* Storing result of Binary Op */
+    set         -52, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 19*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 19*/
+/* Subtracting */
+    set         -52, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* line number 19*/
+/* Storing 9999.50 into  */
+    set         4, %l0
+    add         %fp, %l0, %l0
+    set         float_8, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %f1
+    st          %f1, [%l0]
+    set         4, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fsubs       %f0, %f1, %f3
+/* line number 19*/
+/* Storing result of Binary Op */
+    set         -56, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 20*/
+/* setting wordsworth = float - float */
+    set         -60, %l0
+    add         %fp, %l0, %l0
+    set         -56, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* line number 20*/
+/* Done. */
+/* line number 20*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 20*/
+/* Subtracting */
+    set         shakespeare, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    set         -32, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fsubs       %f0, %f1, %f3
+/* line number 20*/
+/* Storing result of Binary Op */
+    set         -64, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 20*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 20*/
+/* Adding */
+    set         -64, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    set         -48, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fadds       %f0, %f1, %f3
+/* line number 20*/
+/* Storing result of Binary Op */
+    set         -68, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 20*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 20*/
+/* Multiplying */
+    set         -68, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+/* line number 20*/
+/* Storing 1.00 into  */
+    set         4, %l0
+    add         %fp, %l0, %l0
+    set         float_9, %l2
+    add         %g0, %l2, %l2
+    ld          [%l2], %f1
+    st          %f1, [%l0]
+    set         4, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fmuls       %f0, %f1, %f3
+/* line number 20*/
+/* Storing result of Binary Op */
+    set         -72, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 20*/
+/* Prepping Arithmetic Calculations by loading */
     set         -16, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 20*/
+/* Adding */
+    set         -72, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    set         -16, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f1
+    fitos       %f1, %f1
+    fadds       %f0, %f1, %f3
+/* line number 20*/
+/* Storing result of Binary Op */
+    set         -76, %l4
+    add         %fp, %l4, %l4
+    st          %f3, [%l4]
+/* line number 22*/
+/* setting lilwayne = float + int */
+    set         -80, %l0
+    add         %fp, %l0, %l0
+    set         -76, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* line number 22*/
+/* Done. */
+/* line number 22*/
+/* printing float shakespeare */
+    set         shakespeare, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 22*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 23*/
+/* printing float marlowe */
+    set         marlowe, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 23*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 24*/
+/* printing float goethe */
+    set         goethe, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 24*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 25*/
+/* printing float shelley */
+    set         shelley, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 25*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 26*/
+/* printing float byron */
+    set         -32, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
     call    printFloat
     nop
-/* line number 14*/
+/* line number 26*/
 /* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 27*/
+/* printing float tennyson */
+    set         -48, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 27*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 28*/
+/* printing float wordsworth */
+    set         -60, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 28*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 29*/
+/* printing float lilwayne */
+    set         -80, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 29*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 30*/
+/* Printing int twilightSeries */
+    set         _intFmt, %o0
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 30*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 31*/
+/* Printing int reallyReally */
+    set         _intFmt, %o0
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 31*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 32*/
+/* Printing int sucksAlot */
+    set         _intFmt, %o0
+    set         -16, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 32*/
+/* Done printing int. */
     set         _endl, %o0
     call    printf
     nop
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 16) & -8
+SAVE.main = -(92 + 80) & -8
