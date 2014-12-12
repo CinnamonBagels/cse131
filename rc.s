d3963fbae29ac5ecdf51123540a600f66b014606
Tansen Zhu <tansen.zhu@gmail.com>
2014-12-11 18:26:43 -0800

    adfkjsa;flkj

---
+++ rc.s
@@ -0,0 +1,57 @@
+
+! DEFINING INTERNAL CONSTANTS --
+                .section     ".rodata"
+_endl:          .asciz       "\n"        
+_intFmt:        .asciz       "%d"        
+_strFmt:        .asciz       "%s"        
+_boolT:         .asciz       "true"      
+_boolF:         .asciz       "false"     
+rfmt:           .asciz       "%.21f"     
+arrayOutOfBounds:    .asciz       "Index value of %d is outside legal range [0,%d)."
+
+                .section     ".text"
+                .align 4
+                .global      main
+main:
+    set         SAVE.main, %g1
+    save        %sp, %g1, %sp
+    set         globalInit_, %l0
+    ld          [%l0], %l0
+    cmp         %l0, %g0
+    bne     globalInit_end
+    nop
+    set         globalInit_, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+globalInit_end:
+    set         0, %l0
+    cmp         %l0, %g0
+    be      else_0
+    nop
+if_0:
+! --storing constant x with value 1.0
+    set         -8, %l0
+    add         %fp, %l0, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+    ba      endIf_1
+    nop
+else_0:
+endIf_1:
+/* line number 6*/
+/* Printing int x */
+    set         _intFmt, %o0
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 6*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+main_end:
+    ret 
+    restore
+SAVE.main = -(92 + 8) & -8
