bc5fcd531bf0fb0a98ca62b8cea1fcf6f620ffea
cinnamonbagels <samuelsko@gmail.com>
2014-12-11 16:27:50 -0800

    stuff

--- rc.s
+++ rc.s
@@ -1,8 +1,15 @@
 ! --globals--
                 .section     ".data"
                 .align 4
+                 .global     shakespeare,marlowe
+shakespeare:    .single      0r0.0       
+marlowe:        .single      0r0.0       
 globalInit_:    .word        0           
-float_0:        .single      0r5.6       
+float_0:        .single      0r9000.0    
+float_1:        .single      0r5000.0    
+float_2:        .single      0r5.6       
+float_3:        .single      0r9.2       
+float_4:        .single      0r51.52     
 
 ! DEFINING INTERNAL CONSTANTS --
                 .section     ".rodata"
@@ -29,27 +36,39 @@ main:
     set         1, %l1
     st          %l1, [%l0]
 globalInit_end:
-/* line number 7*/
+! --storing constant shakespeare with value 9000.0
+    set         shakespeare, %l0
+    add         %g0, %l0, %l0
+    set         float_0, %l1
+    ld          [%l1], %f1
+    st          %f1, [%l0]
+! --storing constant marlowe with value 5000.0
+    set         marlowe, %l0
+    add         %g0, %l0, %l0
+    set         float_1, %l1
+    ld          [%l1], %f1
+    st          %f1, [%l0]
+/* line number 12*/
 /* setting twilightSeries = 5000 */
     set         5000, %l0
     st          %l0, [%fp-8]
-/* line number 7*/
+/* line number 12*/
 /* Done. */
-/* line number 7*/
+/* line number 12*/
 /* Prepping Arithmetic Calculations by loading */
     set         -8, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %l0
-/* line number 7*/
+/* line number 12*/
 /* Subtracting */
     set         -8, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
-/* line number 7*/
-/* Storing 5.60 into  */
+/* line number 12*/
+/* Storing Arithmetic Op into  */
     set         4, %l0
     add         %fp, %l0, %l0
-    set         float_0, %l2
+    set         float_5, %l2
     add         %g0, %l2, %l2
     ld          [%l2], %f1
     st          %f1, [%l0]
@@ -58,12 +77,12 @@ globalInit_end:
     ld          [%l1], %f1
     fitos       %f0, %f0
     fsubs       %f0, %f1, %f3
-/* line number 7*/
+/* line number 12*/
 /* Storing result of Binary Op */
     set         -12, %l4
     add         %fp, %l4, %l4
     st          %f3, [%l4]
-/* line number 9*/
+/* line number 14*/
 /* setting byron = int - float */
     set         -16, %l0
     add         %fp, %l0, %l0
@@ -71,16 +90,16 @@ globalInit_end:
     add         %fp, %l1, %l1
     ld          [%l1], %f0
     st          %f0, [%l0]
-/* line number 9*/
+/* line number 14*/
 /* Done. */
-/* line number 9*/
+/* line number 14*/
 /* printing float byron */
     set         -16, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
     call    printFloat
     nop
-/* line number 9*/
+/* line number 14*/
 /* Done printing float. */
     set         _endl, %o0
     call    printf
