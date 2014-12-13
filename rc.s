f15e154b1220aa9f3f6e6b434163b82dd5a0d2c9
cinnamonbagels <samuelsko@gmail.com>
2014-12-13 09:33:41 -0800

    doing shit

--- rc.s
+++ rc.s
@@ -2,7 +2,13 @@
                 .section     ".data"
                 .align 4
 FLOAT_FORCE_1:    .single      0r1.0       
-str_0:          .asciz       "testing bitwise or"
+y:              .word        0           
+staticGuard_y:    .word        0           
+main_y:         .word        0           
+staticGuard_main_y:    .word        0           
+str_0:          .asciz       "GOT YOU!"  
+str_1:          .asciz       "YES!"      
+str_2:          .asciz       "YES!"      
 globalInit_:    .word        0           
 
 ! DEFINING INTERNAL CONSTANTS --
@@ -21,53 +27,207 @@ arrayOutOfBounds:    .asciz       "Index value of %d is outside legal range [0,%
 main:
     set         SAVE.main, %g1
     save        %sp, %g1, %sp
+    set         staticGuard_y, %l0
+    ld          [%l0], %l1
+    cmp         %g0, %l1
+    bne     staticGuardLabel_y
+    nop
+    set         y, %l0
+    add         %g0, %l0, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+    set         staticGuard_y, %l2
+    set         1, %l3
+    st          %l3, [%l2]
+staticGuardLabel_y:
+    set         staticGuard_main_y, %l0
+    ld          [%l0], %l1
+    cmp         %g0, %l1
+    bne     staticGuardLabel_main_y
+    nop
+    set         staticGuard_main_y, %l2
+    set         1, %l3
+    st          %l3, [%l2]
+staticGuardLabel_main_y:
     set         globalInit_, %l0
     ld          [%l0], %l0
     cmp         %l0, %g0
     bne     globalInit_end
     nop
+! --storing constant y with value 1.0
     set         globalInit_, %l0
     set         1, %l1
     st          %l1, [%l0]
 globalInit_end:
-/* line number 6*/
+/* line number 8*/
+/* setting x = 0 */
+    set         0, %l0
+    st          %l0, [%fp-8]
+/* line number 8*/
+/* Done. */
+! --storing constant y with value 1.0
+    set         main_y, %l0
+    add         %g0, %l0, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+/* line number 11*/
+/* Prepping Comparison Calculations by loading */
+/* line number 11*/
+/* Loading x to %l0 */
+    set         0, %l0
+/* line number 11*/
+/* Loading y to %l1 */
+    set         y, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l1
+/* line number 11*/
+/* Starting greater than equal */
+    cmp         %l0, %l1
+    bge     greaterEqual_0
+    nop
+    set         0, %l3
+    ba      greaterEqualEnd_0
+    nop
+greaterEqual_0:
+    set         1, %l3
+greaterEqualEnd_0:
+/* line number 11*/
+/* Storing result of Comparison Op */
+    set         -12, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 12*/
+/* Loading Validating ComparisonOp int and int as a BooleanType for operator: >=...
+ to %l0 */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    cmp         %l0, %g0
+    be      else_0
+    nop
+if_0:
+/* line number 13*/
 /* printing string */
     set         _strFmt, %o0
     set         str_0, %o1
     call    printf
     nop
-/* line number 6*/
+/* line number 13*/
 /* Done printing string. */
     set         _endl, %o0
     call    printf
     nop
-/* line number 7*/
-/* Prepping Arithmetic Calculations by loading */
-/* line number 7*/
-/* Loading 1 to %l0 */
-    set         1, %l0
-/* line number 7*/
-/* Loading 1 to %l1 */
+    ba      endIf_1
+    nop
+else_0:
+endIf_1:
+/* line number 16*/
+/* Prepping Comparison Calculations by loading */
+/* line number 16*/
+/* Loading x to %l0 */
+    set         0, %l0
+/* line number 16*/
+/* Loading x to %l1 */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l1
+/* line number 16*/
+/* Starting == equal */
+    cmp         %l0, %l1
+    be      equal_1
+    nop
+    set         0, %l3
+    ba      equalEnd_1
+    nop
+equal_1:
+    set         1, %l3
+equalEnd_1:
+/* line number 16*/
+/* Storing result of Comparison Op */
+    set         -16, %l4
+    add         %fp, %l4, %l4
+    st          %l3, [%l4]
+/* line number 17*/
+/* Loading Validating ComparisonOp int and int as a BooleanType for operator: ==...
+ to %l0 */
+    set         -16, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %l0
+    cmp         %l0, %g0
+    be      else_2
+    nop
+if_2:
+/* line number 18*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_1, %o1
+    call    printf
+    nop
+/* line number 18*/
+/* Done printing string. */
+    set         _endl, %o0
+    call    printf
+    nop
+    ba      endIf_3
+    nop
+else_2:
+endIf_3:
+/* line number 21*/
+/* Short-circuiting && with true */
+/* line number 21*/
+/* Loading true to %l1 */
     set         1, %l1
-/* line number 7*/
-/* Oring */
-    or          %l0, %l1, %l2
-/* line number 7*/
+    cmp         %l1, %g0
+    be      andF_0
+    nop
+/* line number 21*/
+/* Prepping Arithmetic Calculations by loading */
+/* line number 21*/
+/* Loading true to %l0 */
+    set         1, %l0
+/* line number 21*/
+/* Loading true to %l1 */
+    set         1, %l1
+/* line number 21*/
+/* &&-ing */
+    cmp         %l1, %g0
+    bne     andT_0
+    nop
+andF_0:
+    set         0, %l2
+    ba      andEnd_0
+    nop
+andT_0:
+    set         1, %l2
+andEnd_0:
+/* line number 21*/
 /* Storing result of Binary Op */
     set         1, %l4
     add         %g0, %l4, %l4
     st          %l2, [%l4]
-/* line number 7*/
-/* printf on int */
-    set         _intFmt, %o0
-    set         1, %o1
+/* line number 22*/
+/* Loading true to %l0 */
+    set         1, %l0
+    cmp         %l0, %g0
+    be      else_4
+    nop
+if_4:
+/* line number 23*/
+/* printing string */
+    set         _strFmt, %o0
+    set         str_2, %o1
     call    printf
     nop
-
+/* line number 23*/
+/* Done printing string. */
     set         _endl, %o0
     call    printf
     nop
+    ba      endIf_5
+    nop
+else_4:
+endIf_5:
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 4) & -8
+SAVE.main = -(92 + 16) & -8
