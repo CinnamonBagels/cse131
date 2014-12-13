60f0da36d6b45cfa235d40562ef86f5df390f443
cinnamonbagels <samuelsko@gmail.com>
2014-12-13 06:34:44 -0800

    deleting

---
+++ rc.s
@@ -0,0 +1,62 @@
+! --globals--
+                .section     ".data"
+                .align 4
+FLOAT_FORCE_1:    .single      0r1.0       
+                 .global     food
+float_0:        .single      0r4.0       
+globalInit_:    .word        0           
+                .section     ".bss"
+                .align 4
+food:           .skip        12          
+
+
+! DEFINING INTERNAL CONSTANTS --
+                .section     ".rodata"
+_endl:          .asciz       "\n"        
+_intFmt:        .asciz       "%d"        
+_strFmt:        .asciz       "%s"        
+_boolT:         .asciz       "true"      
+_boolF:         .asciz       "false"     
+rfmt:           .asciz       "%.21f"     
+arrayOutOfBounds:    .asciz       "Index value of %d is outside legal range [0,%d)."
+
+                .section     ".text"
+                .align 4
+                .global      main
+main:
+    set         SAVE.main, %g1
+    save        %sp, %g1, %sp
+    set         globalInit_, %l0
+    ld          [%l0], %l0
+    cmp         %l0, %g0
+    bne     globalInit_end
+    nop
+    set         globalInit_, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+globalInit_end:
+! --storing constant a with value 4.0
+    set         -8, %l0
+    add         %fp, %l0, %l0
+    set         float_0, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* line number 12*/
+/* printing float STO.VarSTO@36527f */
+/* line number 12*/
+/* Loading a to %f0 */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 12*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+main_end:
+    ret 
+    restore
+SAVE.main = -(92 + 4) & -8
