bd1ed054432ffd8ceb37991c26dc5da41e157693
cinnamonbagels <samuelsko@gmail.com>
2014-12-13 07:04:07 -0800

    stuff

--- rc.s
+++ rc.s
@@ -4,6 +4,8 @@
 FLOAT_FORCE_1:    .single      0r1.0       
                  .global     food,bed
 float_0:        .single      0r88.43     
+float_1:        .single      0r22.31     
+float_2:        .single      0r11.55     
 globalInit_:    .word        0           
                 .section     ".bss"
                 .align 4
@@ -38,13 +40,27 @@ main:
     st          %l1, [%l0]
 globalInit_end:
 ! --storing constant a with value 88.43
-    set         -4, %l0
+    set         -8, %l0
     add         %fp, %l0, %l0
     set         float_0, %l1
     add         %g0, %l1, %l1
     ld          [%l1], %f0
     st          %f0, [%l0]
-/* line number 22*/
+! --storing constant b with value 22.31
+    set         -4, %l0
+    add         %fp, %l0, %l0
+    set         float_1, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+! --storing constant c with value 11.55
+    set         -12, %l0
+    add         %fp, %l0, %l0
+    set         float_2, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %f0
+    st          %f0, [%l0]
+/* line number 25*/
 /* Assigning struct food to food1 */
     set         food, %o1
     add         %g0, %o1, %o1
@@ -53,7 +69,7 @@ globalInit_end:
     set         12, %o2
     call    memcpy
     nop
-/* line number 23*/
+/* line number 26*/
 /* Assigning struct food1 to food2 */
     set         -16, %o1
     add         %fp, %o1, %o1
@@ -62,16 +78,53 @@ globalInit_end:
     set         12, %o2
     call    memcpy
     nop
-/* line number 26*/
-/* printing float STO.VarSTO@b6548 */
-/* line number 26*/
+/* line number 27*/
+/* Assigning struct food2 to food3 */
+    set         -28, %o1
+    add         %fp, %o1, %o1
+    set         -40, %o0
+    add         %fp, %o0, %o0
+    set         12, %o2
+    call    memcpy
+    nop
+/* line number 29*/
+/* printing float STO.VarSTO@269997 */
+/* line number 29*/
 /* Loading a to %f0 */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 29*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 30*/
+/* printing float STO.VarSTO@2db19d */
+/* line number 30*/
+/* Loading b to %f0 */
     set         -4, %l1
     add         %fp, %l1, %l1
     ld          [%l1], %f0
     call    printFloat
     nop
-/* line number 26*/
+/* line number 30*/
+/* Done printing float. */
+    set         _endl, %o0
+    call    printf
+    nop
+/* line number 31*/
+/* printing float STO.VarSTO@1e7c5cb */
+/* line number 31*/
+/* Loading c to %f0 */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %f0
+    call    printFloat
+    nop
+/* line number 31*/
 /* Done printing float. */
     set         _endl, %o0
     call    printf
@@ -79,4 +132,4 @@ globalInit_end:
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 28) & -8
+SAVE.main = -(92 + 40) & -8
