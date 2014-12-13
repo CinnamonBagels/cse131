d47fb2e523884f6df66d6297724a3ee880575f6b
cinnamonbagels <samuelsko@gmail.com>
2014-12-12 22:52:59 -0800

    stuff

--- rc.s
+++ rc.s
@@ -68,16 +68,16 @@ globalInit_end:
     add         %g0, %l1, %l1
     ld          [%l1], %l1
 /* line number 11*/
-/* Starting not equal */
+/* Starting Less than */
     cmp         %l0, %l1
-    bne     nEqual_0
+    bl      less_0
     nop
     set         0, %l3
-    ba      nEqualEnd_0
+    ba      lessEnd_0
     nop
-nEqual_0:
+less_0:
     set         1, %l3
-nEqualEnd_0:
+lessEnd_0:
 /* line number 11*/
 /* Storing result of Comparison Op */
     set         -16, %l4
@@ -102,7 +102,7 @@ negEnd_0:
     add         %fp, %l1, %l1
     st          %l0, [%l1]
 /* line number 11*/
-/* Printing bool Validating UnaryOp Validating ComparisonOp int and int as a BooleanType for operator: !=...
+/* Printing bool Validating UnaryOp Validating ComparisonOp int and int as a BooleanType for operator: <...
  as a BooleanType for operator: !...
  */
     set         -20, %l1
@@ -126,39 +126,7 @@ branchEnd_0:
     set         _endl, %o0
     call    printf
     nop
-    set         _strFmt, %o0
-    set         _boolF, %o1
-    call    printf
-    nop
-
-    set         _endl, %o0
-    call    printf
-    nop
-    set         _strFmt, %o0
-    set         _boolT, %o1
-    call    printf
-    nop
-
-    set         _endl, %o0
-    call    printf
-    nop
-! --storing constant x with value 5.0
-    set         x, %l0
-    add         %g0, %l0, %l0
-    set         x, %l0
-    add         %g0, %l0, %l0
-    ld          [%l0], %l0
-    set         5, %l1
-    st          %l1, [%l0]
-! --storing constant y with value 5.0
-    set         y, %l0
-    add         %g0, %l0, %l0
-    set         y, %l0
-    add         %g0, %l0, %l0
-    ld          [%l0], %l0
-    set         5, %l1
-    st          %l1, [%l0]
-/* line number 17*/
+/* line number 12*/
 /* Prepping Comparison Calculations by loading */
     set         x, %l1
     add         %g0, %l1, %l1
@@ -166,23 +134,23 @@ branchEnd_0:
     set         y, %l1
     add         %g0, %l1, %l1
     ld          [%l1], %l1
-/* line number 17*/
-/* Starting Less than */
+/* line number 12*/
+/* Starting == equal */
     cmp         %l0, %l1
-    bl      less_1
+    be      equal_1
     nop
     set         0, %l3
-    ba      lessEnd_1
+    ba      equalEnd_1
     nop
-less_1:
+equal_1:
     set         1, %l3
-lessEnd_1:
-/* line number 17*/
+equalEnd_1:
+/* line number 12*/
 /* Storing result of Comparison Op */
     set         -24, %l4
     add         %fp, %l4, %l4
     st          %l3, [%l4]
-/* line number 17*/
+/* line number 12*/
 /* Negating */
     set         -24, %l1
     add         %fp, %l1, %l1
@@ -200,8 +168,8 @@ negEnd_1:
     set         -28, %l1
     add         %fp, %l1, %l1
     st          %l0, [%l1]
-/* line number 17*/
-/* Printing bool Validating UnaryOp Validating ComparisonOp int and int as a BooleanType for operator: <...
+/* line number 12*/
+/* Printing bool Validating UnaryOp Validating ComparisonOp int and int as a BooleanType for operator: ==...
  as a BooleanType for operator: !...
  */
     set         -28, %l1
@@ -220,74 +188,7 @@ printFalse_1:
 branchEnd_1:
     call    printf
     nop
-/* line number 17*/
-/* Done printing bool. */
-    set         _endl, %o0
-    call    printf
-    nop
-/* line number 18*/
-/* Prepping Comparison Calculations by loading */
-    set         x, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %l0
-    set         y, %l1
-    add         %g0, %l1, %l1
-    ld          [%l1], %l1
-/* line number 18*/
-/* Starting == equal */
-    cmp         %l0, %l1
-    be      equal_2
-    nop
-    set         0, %l3
-    ba      equalEnd_2
-    nop
-equal_2:
-    set         1, %l3
-equalEnd_2:
-/* line number 18*/
-/* Storing result of Comparison Op */
-    set         -32, %l4
-    add         %fp, %l4, %l4
-    st          %l3, [%l4]
-/* line number 18*/
-/* Negating */
-    set         -32, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    cmp         %l0, %g0
-    be      increment_2
-    nop
-decrement_2:
-    dec     %l0
-    ba      negEnd_2
-    nop
-increment_2:
-    inc     %l0
-negEnd_2:
-    set         -36, %l1
-    add         %fp, %l1, %l1
-    st          %l0, [%l1]
-/* line number 18*/
-/* Printing bool Validating UnaryOp Validating ComparisonOp int and int as a BooleanType for operator: ==...
- as a BooleanType for operator: !...
- */
-    set         -36, %l1
-    add         %fp, %l1, %l1
-    ld          [%l1], %l0
-    set         _strFmt, %o0
-    cmp         %l0, %g0
-    be      printFalse_2
-    nop
-printTrue_2:
-    set         _boolT, %o1
-    ba      branchEnd_2
-    nop
-printFalse_2:
-    set         _boolF, %o1
-branchEnd_2:
-    call    printf
-    nop
-/* line number 18*/
+/* line number 12*/
 /* Done printing bool. */
     set         _endl, %o0
     call    printf
@@ -295,4 +196,4 @@ branchEnd_2:
 main_end:
     ret 
     restore
-SAVE.main = -(92 + 36) & -8
+SAVE.main = -(92 + 28) & -8
