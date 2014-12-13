8f27f6bbcbe23075d72585be8c8922a144544b64
Tansen Zhu <tansen.zhu@gmail.com>
2014-12-13 04:52:57 -0800

    testT

---
+++ rc.s
@@ -0,0 +1,92 @@
+! --globals--
+                .section     ".data"
+                .align 4
+FLOAT_FORCE_1:    .single      0r1.0       
+globalInit_:    .word        0           
+
+! DEFINING INTERNAL CONSTANTS --
+                .section     ".rodata"
+_endl:          .asciz       "\n"        
+_intFmt:        .asciz       "%d"        
+_strFmt:        .asciz       "%s"        
+_boolT:         .asciz       "true"      
+_boolF:         .asciz       "false"     
+rfmt:           .asciz       "%.21f"     
+arrayOutOfBounds:    .asciz       "Index value of %d is outside legal range [0,%d)."
+
+                .section     ".text"
+                .align 4
+                .global      foo
+foo:
+    set         SAVE.foo, %g1
+    save        %sp, %g1, %sp
+/* line number 2*/
+/* Storing parameter x */
+    st          %i0, [%fp+-8]
+/* line number 4*/
+/* Returning value from foo */
+/* line number 4*/
+/* Loading x to %i0 */
+    set         -8, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %i0
+    ba      foo_end
+    nop
+foo_end:
+    ret 
+    restore
+SAVE.foo = -(92 + 4) & -8
+                .section     ".text"
+                .align 4
+                .global      main
+main:
+    set         SAVE.main, %g1
+    save        %sp, %g1, %sp
+    set         globalInit_, %l0
+    ld          [%l0], %l0
+    cmp         %l0, %g0
+    bne     globalInit_end
+    nop
+    set         globalInit_, %l0
+    set         1, %l1
+    st          %l1, [%l0]
+globalInit_end:
+/* line number 8*/
+/* Loading foo to %l0 */
+    set         foo, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l0
+    call    foo
+    nop
+/* line number 8*/
+/* Saving return value */
+    st          %o0, [%fp+-8]
+/* line number 8*/
+/* Loading foo to %l0 */
+    set         foo, %l1
+    add         %g0, %l1, %l1
+    ld          [%l1], %l0
+    call    foo
+    nop
+/* line number 8*/
+/* Saving return value */
+    st          %o0, [%fp+-12]
+/* line number 8*/
+/* Printing int foo() */
+    set         _intFmt, %o0
+/* line number 8*/
+/* Loading foo() to %o1 */
+    set         -12, %l1
+    add         %fp, %l1, %l1
+    ld          [%l1], %o1
+    call    printf
+    nop
+/* line number 8*/
+/* Done printing int. */
+    set         _endl, %o0
+    call    printf
+    nop
+main_end:
+    ret 
+    restore
+SAVE.main = -(92 + 12) & -8
