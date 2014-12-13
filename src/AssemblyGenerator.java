550f7acd3e9ec7859c2479cb7bca3337c802a88f
cinnamonbagels <samuelsko@gmail.com>
2014-12-12 18:30:03 -0800

    jesus this is hard

--- src/AssemblyGenerator.java
+++ src/AssemblyGenerator.java
@@ -512,8 +512,14 @@ public class AssemblyGenerator {
 			//setting value.
 			generateASM(Strings.two_param, Instructions.set, value.offset, Registers.l3);
 			generateASM(Strings.three_param, Instructions.add, value.base, Registers.l3, Registers.l3);
-			generateASM(Strings.two_param, Instructions.load, "[" + Registers.l3 + "]", dest_register);
-			generateASM(Strings.two_param, Instructions.store, dest_register, "[" + Registers.l5 + "]");
+			if(value.getType().isFloat()) {
+				generateASM(Strings.two_param, Instructions.load, "[" + Registers.l3 + "]", dest_register);
+				generateASM(Strings.two_param, Instructions.store, dest_register, "[" + Registers.l5 + "]");
+			} else {
+				generateASM(Strings.two_param, Instructions.load, "[" + Registers.l3 + "]", Registers.l3);
+				generateASM(Strings.two_param, Instructions.store, Registers.l3, "[" + dest_register + "]");
+			}
+			
 
 		}
 		
