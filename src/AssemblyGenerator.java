925f5eb738ce1852ccea4f1fbf44fc011f3367ba
cinnamonbagels <samuelsko@gmail.com>
2014-12-14 20:11:49 -0800

    final turnin?

--- src/AssemblyGenerator.java
+++ src/AssemblyGenerator.java
@@ -72,7 +72,7 @@ public class AssemblyGenerator {
 		write(assembleString(Strings.init, Strings.boolf + ":", ".asciz", "\"false\""));
 		write(assembleString(Strings.init, Strings.rfmt + ":", Strings.asciz, Strings.floatFormat));
 		write(assembleString(Strings.init, "arrayOutOfBounds:", Strings.asciz, "\"" + "Index value of %d is outside legal range [0,%d)." + "\""));
-		write(assembleString(Strings.init, "nullptrexception", Strings.asciz, "\"" + "Attempt to dereference NULL pointer." + "\""));
+		write(assembleString(Strings.init, "nullptrexception:", Strings.asciz, "\"" + "Attempt to dereference NULL pointer." + "\""));
 		write(assembleString("\n"));
 		
 	}
